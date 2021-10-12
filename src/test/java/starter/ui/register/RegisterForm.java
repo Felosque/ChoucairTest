c43a199cc2e5a552f0aa8842be6dc11b4bca4416
Felosque <felipelondono4@gmail.com>
2021-10-12 10:18:58 -0500

    added register.feature

--- src/test/java/starter/ui/register/RegisterForm.java
+++ src/test/java/starter/ui/register/RegisterForm.java
@@ -6,5 +6,6 @@ public class RegisterForm {
 
     public static By REGISTER_BUTTON = By.id("SubmitCreate");
     public static By REGISTER_EMAIL = By.id("email_create");
+    public static RegisterFieldsForm registerFieldsForm;
 
 }
